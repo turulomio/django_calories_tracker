de3733468face7dd91cdb9f41cecf36c3f028122
turulomio <turulomio@yahoo.es>
2022-04-11 14:41:36 +0200

    Added hardcoded strings

---
+++ calories_tracker/hardcoded_strings.py
@@ -0,0 +1,4 @@
+from django.utils.translation import gettext_lazy as _
+_('Gain weight')
+_('Lose weight')
+_('Mantain weight')
--- calories_tracker/management/commands/makedbmessages.py
+++ calories_tracker/management/commands/makedbmessages.py
@@ -1,4 +1,4 @@
-from moneymoney.reusing.connection_dj import cursor_rows
+from calories_tracker.reusing.connection_dj import cursor_rows
 from django.core.management.base import BaseCommand
 
 
@@ -7,23 +7,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         strings=[]
-        for row in cursor_rows("select name from concepts where editable is false order by name"):
+        for row in cursor_rows("select name from weight_wishes order by name"):
             strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from leverages order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from productstypes order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from operationstypes order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from stockmarkets order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from banks where id=3"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from accounts where id=4"):
-            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from leverages order by name"):
+#            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from productstypes order by name"):
+#            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from operationstypes order by name"):
+#            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from stockmarkets order by name"):
+#            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from banks where id=3"):
+#            strings.append("_('{}')".format(row["name"]))
+#        for row in cursor_rows("select name from accounts where id=4"):
+#            strings.append("_('{}')".format(row["name"]))
             
         strings.sort()
-        f=open("moneymoney/hardcoded_strings.py", "w")
+        f=open("calories_tracker/hardcoded_strings.py", "w")
         f.write("from django.utils.translation import gettext_lazy as _\n")
         for s in strings:
             f.write(f"{s}\n")
