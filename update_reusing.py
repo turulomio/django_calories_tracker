3e5966b725bab67ad1810dc9d6ca478d3bb0416d
turulomio <turulomio@yahoo.es>
2022-04-11 10:17:51 +0200

    Added update_reusing

--- update_reusing.py
+++ update_reusing.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from moneymoney.reusing.github import download_from_github
+from calories_tracker.reusing.github import download_from_github
 from os import remove
 
 def replace_in_file(filename, s, r):
@@ -15,8 +15,19 @@ parser.add_argument('--local', help='Parses files without download', action="sto
 args=parser.parse_args()      
 
 if args.local==False:
-    download_from_github("turulomio", "reusingcode", "python/github.py", "moneymoney/reusing")
-    download_from_github("turulomio", "reusingcode", "python/datetime_functions.py", "moneymoney/reusing")
-    download_from_github("turulomio", "reusingcode", "python/text_inputs.py", "moneymoney/reusing")
+    download_from_github("turulomio", "reusingcode", "python/call_by_name.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/connection_pg.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/listdict_functions.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "django/decorators.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/lineal_regression.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/casts.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/currency.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/github.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/datetime_functions.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/text_inputs.py", "calories_tracker/reusing")
+    download_from_github("turulomio", "reusingcode", "python/libmanagers.py", "calories_tracker/reusing")
 
+replace_in_file("calories_tracker/reusing/casts.py", "from currency", "from .currency")
+replace_in_file("calories_tracker/reusing/casts.py", "from percentage", "from .percentage")
+replace_in_file("calories_tracker/reusing/listdict_functions.py", "from casts", "from .casts")
 
