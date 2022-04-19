2f823d3d729f51c8105a165111e52d76404ef8ca
turulomio <turulomio@yahoo.es>
2022-04-19 17:56:31 +0200

    Added catalog manager settings

--- django_calories_tracker/settings.py
+++ django_calories_tracker/settings.py
@@ -139,7 +139,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -149,3 +148,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CATALOG_MANAGER = True
