be70873ed9eb71bf1041607032bb6d849aeefab1
turulomio <turulomio@yahoo.es>
2022-11-26 13:15:27 +0100

    IMproving translations

--- calories_tracker/management/commands/makedbmessages.py
+++ calories_tracker/management/commands/makedbmessages.py
@@ -17,6 +17,8 @@ class Command(BaseCommand):
             strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from formats order by name"):
             strings.append("_('{}')".format(row["name"]))
+        for row in cursor_rows("select name from recipes_links_types order by name"):
+            strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from system_products where system_companies_id is null order by name"):
             strings.append("_('{}')".format(row["name"]))
             
