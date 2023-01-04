fbc4f4208c37937b29d72c56d8d58da79730d955
turulomio <turulomio@yahoo.es>
2023-01-04 07:08:03 +0100

    Removed pagination global settings

--- django_calories_tracker/settings.py
+++ django_calories_tracker/settings.py
@@ -25,8 +25,7 @@ REST_FRAMEWORK={
     ], 
     'COERCE_DECIMAL_TO_STRING': False, 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+
 }
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Django Calories Tracker API Documentation',
