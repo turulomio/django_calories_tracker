e061febde13bf1c6a6e0b0e54bc3612d6b35c2c2
turulomio <turulomio@yahoo.es>
2023-09-11 04:34:41 +0200

    Removing steps, stir and temperatures

--- calories_tracker/management/commands/makedbmessages.py
+++ calories_tracker/management/commands/makedbmessages.py
@@ -19,15 +19,9 @@ class Command(BaseCommand):
             strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from recipes_links_types order by name"):
             strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from stir_types order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from temperatures_types order by name"):
-            strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from recipes_categories order by name"):
             strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from measures_types order by name"):
-            strings.append("_('{}')".format(row["name"]))
-        for row in cursor_rows("select name from steps order by name"):
             strings.append("_('{}')".format(row["name"]))
         for row in cursor_rows("select name from system_products where system_companies_id is null order by name"):
             strings.append("_('{}')".format(row["name"]))
