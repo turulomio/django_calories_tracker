bf7288b57992eaa6e6a1d38ca46cdf07bf35f056
turulomio <turulomio@yahoo.es>
2023-12-08 06:44:40 +0100

    Set max page limit to 100

--- calories_tracker/paginators.py
+++ calories_tracker/paginators.py
@@ -5,7 +5,7 @@ from .reusing.request_casting import RequestGetBool
 class PagePaginationWithTotalPages(PageNumberPagination):
     page_size = 10
     page_size_query_param="itemsPerPage"
-    max_page_size = 30
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response({
