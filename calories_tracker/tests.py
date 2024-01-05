e39fc23cd2260f1e249b607a83aa5ae153a27145
turulomio <turulomio@yahoo.es>
2024-01-05 17:40:51 +0100

    Now you can merge recipes. Poetry update (#13)
    * First try
    
    * poetry update
    
    * Added test for recipe merge
    
    * Changed queryset for list only

--- calories_tracker/tests.py
+++ calories_tracker/tests.py
@@ -254,7 +254,6 @@ class CtTestCase(APITestCase):
     def test_pots(self):
         tests_helpers.common_tests_Private(self,  '/api/pots/', models.Pots.post_payload(),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
     
-    @tag("current")
     def test_products(self):
         tests_helpers.common_tests_Private(self,  '/api/products/', models.Products.post_payload(),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
         
@@ -334,10 +333,31 @@ class CtTestCase(APITestCase):
         dict_found=tests_helpers.client_get(self, self.client_authorized_1, "/api/system_products/?search=Zucchini", status.HTTP_200_OK)
         self.assertEqual(len(dict_found),1 )
 
-                                         
+    @tag("current")
     def test_recipes(self):
         tests_helpers.common_tests_Private(self,  '/api/recipes/', models.Recipes.post_payload(),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
-                                         
+
+        #Merge recipes
+        dict_recipe_main=tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes/", models.Recipes.post_payload(),  status.HTTP_201_CREATED)
+        self.assertEqual(len(dict_recipe_main["elaborations"]), 0)
+        self.assertEqual(len(dict_recipe_main["recipes_links"]), 0)
+        dict_recipe_1=tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes/", models.Recipes.post_payload(),  status.HTTP_201_CREATED)
+        tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes_links/", models.RecipesLinks.post_payload(recipes=dict_recipe_1["url"]),  status.HTTP_201_CREATED)
+        tests_helpers.client_post(self, self.client_authorized_1,  "/api/elaborations/", models.Elaborations.post_payload(recipes=dict_recipe_1["url"]),  status.HTTP_201_CREATED)
+        dict_recipe_2=tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes/", models.Recipes.post_payload(),  status.HTTP_201_CREATED)
+        tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes_links/", models.RecipesLinks.post_payload(recipes=dict_recipe_2["url"]),  status.HTTP_201_CREATED)
+        tests_helpers.client_post(self, self.client_authorized_1,  "/api/elaborations/", models.Elaborations.post_payload(recipes=dict_recipe_2["url"]),  status.HTTP_201_CREATED)
+
+        dict_merged=tests_helpers.client_post(self, self.client_authorized_1, dict_recipe_main["url"]+"merge/", {"recipes":[dict_recipe_1["url"], dict_recipe_2["url"]]},  status.HTTP_200_OK)
+        ## Checks that have 2 elaborations and recipes_links
+        self.assertEqual(len(dict_merged["elaborations"]), 2)
+        self.assertEqual(len(dict_merged["recipes_links"]), 2)
+        
+        ## Checks that merged recipes are deleted
+        tests_helpers.client_get(self, self.client_authorized_1, dict_recipe_1["url"], status.HTTP_404_NOT_FOUND)        
+        tests_helpers.client_get(self, self.client_authorized_1, dict_recipe_2["url"], status.HTTP_404_NOT_FOUND)
+
+
     def test_recipes_links(self):             
         dict_recipe=tests_helpers.client_post(self, self.client_authorized_1, "/api/recipes/", models.Recipes.post_payload(), status.HTTP_201_CREATED)
         tests_helpers.common_tests_Private(self,  '/api/recipes_links/', models.RecipesLinks.post_payload(recipes=dict_recipe["url"]),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
