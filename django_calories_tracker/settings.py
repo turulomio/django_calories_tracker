14d8579e442c0dd71d0d5bb642121209daf188f5
turulomio <turulomio@yahoo.es>
2024-08-11 20:15:09 +0200

    Github action to test app works (#23)
    * Adding exiftool
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying

--- django_calories_tracker/settings.py
+++ django_calories_tracker/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'calories_tracker',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
