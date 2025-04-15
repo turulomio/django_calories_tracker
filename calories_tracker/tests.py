f0f903d472bc3983886df9738137330ddad060c5
turulomio <turulomio@yahoo.es>
2025-04-15 22:25:03 +0200

    Improving tests

--- calories_tracker/tests.py
+++ calories_tracker/tests.py
@@ -247,21 +247,30 @@ class CtTestCase(APITestCase):
                 
     @tag("current")
     def test_pill_events(self):
+        # Common vars
+        pillname="Pill name"
+        dt_from=timezone.now()
+        days=5
         #LIST NOT STANDARD tests_helpers.common_tests_Private(self,  '/api/pill_events/', models.PillEvents.post_payload(),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
         
         # Removes pillevents from dt. Round timezones use 1 second minus
         #deleted=tests_helpers.client_post(self, self.client_authorized_1,  '/api/pill_events/delete_from_dt/',  {"pillname": "Pill name",  "dt_from": timezone.now()-timedelta(hours=1)},  status.HTTP_200_OK)        
 
-        # Common vars
-        pillname="Pill name"
-        dt_from=timezone.now()
-        days=5
+        # POST
+        tests_helpers.client_post(self, self.client_authorized_1,  '/api/pill_events/', models.PillEvents.post_payload(), status.HTTP_201_CREATED)
+
+        # LIST
+        tests_helpers.client_get(self, self.client_authorized_1, '/api/pill_events/', status.HTTP_400_BAD_REQUEST)
+        lod_pe=tests_helpers.client_get(self, self.client_authorized_1, f'/api/pill_events/?year={dt_from.year}&month={dt_from.month}', status.HTTP_200_OK)
+        self.assertEqual(len(lod_pe), 1)
+        # DELETE
+        tests_helpers.client_delete(self, self.client_authorized_1, lod_pe[0]["url"], {}, status.HTTP_204_NO_CONTENT)
+
+
         
         # Set pillevents each dt
         lod_pe=tests_helpers.client_post(self, self.client_authorized_1,  '/api/pill_events/set_each_day/',  {"pillname": pillname,  "dt_from": dt_from, "days": days},  status.HTTP_200_OK)
         self.assertEqual(len(lod_pe), 5)
-        lod_pe=tests_helpers.client_get(self, self.client_authorized_1, '/api/pill_events/', status.HTTP_400_BAD_REQUEST)
-        lod_pe=tests_helpers.client_get(self, self.client_authorized_1, f'/api/pill_events/?year={dt_from.year}&month={dt_from.month}', status.HTTP_200_OK)
 
         deleted=tests_helpers.client_post(self, self.client_authorized_1,  '/api/pill_events/delete_from_dt/',  {"pillname": pillname,  "dt_from": dt_from-timedelta(seconds=1)},  status.HTTP_200_OK)        
         self.assertEqual(deleted[0], 5)
