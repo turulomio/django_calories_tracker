1dafc0229705a9d808521ac50be1877f4d23f7c0
turulomio <turulomio@yahoo.es>
2025-12-06 07:13:43 +0100

    Seem to work

--- calories_tracker/tests/__init__.py
+++ calories_tracker/tests/__init__.py
@@ -1,54 +1,26 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient, APITestCase
-
 
 class CaloriesTrackerAPITestCase(APITestCase):
     """
     Base class for API tests.
     It sets up fixtures and authenticated clients once for all inheriting test classes.
     """
-    fixtures = ["all.json"]
-
+    fixtures = ["all.json","test_users.json"]
     _setup_done = False
 
     @classmethod
     def setUpClass(cls):
         """
-        Set up data for all test classes.
-        This method is run once for the first test class that inherits from it.
+        Set up non-database specific resources once for all test classes.
         """
         super().setUpClass()
-
         if cls._setup_done:
             return
 
-        # User to test api
-        cls.user_authorized_1 = User.objects.create_user(
-            username='authorized_1',
-            password='testing123',
-            email='testing@testing.com',
-            first_name='Testing',
-            last_name='Testing',
-        )
-
-        # User to confront security
-        cls.user_authorized_2 = User.objects.create_user(
-            username='authorized_2',
-            password='other123',
-            email='other@other.com',
-            first_name='Other',
-            last_name='Other',
-        )
-
-        # User to test api
-        cls.user_catalog_manager = User.objects.create_user(
-            username='catalog_manager',
-            password='catalog_manager123',
-            email='catalog_manager@catalog_manager.com',
-            first_name='Catalog',
-            last_name='Manager',
-        )
-        cls.user_catalog_manager.groups.add(Group.objects.get(name='CatalogManager'))
+        cls.user_authorized_1 = User.objects.get(username='authorized_1')
+        cls.user_authorized_2 = User.objects.get(username='authorized_2')
+        cls.user_catalog_manager = User.objects.get(username='catalog_manager')
 
         cls.client_authorized_1 = APIClient()
         cls.client_authorized_1.force_authenticate(user=cls.user_authorized_1)
@@ -66,4 +38,3 @@ class CaloriesTrackerAPITestCase(APITestCase):
         cls.client_catalog_manager.user = cls.user_catalog_manager
 
         cls._setup_done = True
-        print("LOADED")
