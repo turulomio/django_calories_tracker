f68e6d5372fe2e62d88d844209b9e68ee0b28ebf
turulomio <turulomio@yahoo.es>
2025-12-06 14:01:58 +0100

    Test to check last updaet

--- calories_tracker/tests/test_elaborations_experiences.py
+++ calories_tracker/tests/test_elaborations_experiences.py
@@ -1,17 +1,16 @@
 from calories_tracker import models
-from . import tests_helpers, CaloriesTrackerAPITestCase
-from datetime import date, timedelta
-from django.test import tag
-from django.utils import timezone
-from json import loads
-from pydicts import lod
+from . import tests_helpers
 from rest_framework import status
-
-
-tag, models
 
 def test_elaborations_experiences(self):     
     dict_recipes=tests_helpers.client_post(self, self.client_authorized_1, "/api/recipes/", models.Recipes.post_payload(), status.HTTP_201_CREATED)
     dict_elaborations=tests_helpers.client_post(self, self.client_authorized_1, "/api/elaborations/", models.Elaborations.post_payload(recipes=dict_recipes["url"]), status.HTTP_201_CREATED)
+    
+    # Check all
     tests_helpers.common_tests_Private(self,  '/api/elaborations_experiences/', models.ElaborationsExperiences.post_payload(elaborations=dict_elaborations["url"]),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
 
+    # Check recipe last update has changed upter post
+    dict_recipes_berfore=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
+    dict_elaborations_experience=tests_helpers.client_post(self, self.client_authorized_1, "/api/elaborations_experiences/", models.ElaborationsExperiences.post_payload(elaborations=dict_elaborations["url"]), status.HTTP_201_CREATED)
+    dict_recipes_after=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
+    self.assertNotEqual(dict_recipes_berfore["last"], dict_recipes_after["last"])   
