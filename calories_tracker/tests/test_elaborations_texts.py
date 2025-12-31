f046a93161acc7790b60fac0cb0b1647ae7126ab
turulomio <turulomio@yahoo.es>
2025-12-31 11:19:56 +0100

    Bad

--- calories_tracker/tests/test_elaborations_texts.py
+++ calories_tracker/tests/test_elaborations_texts.py
@@ -6,11 +6,15 @@ def test_elaborations_texts(self):
     dict_recipes=tests_helpers.client_post(self, self.client_authorized_1, "/api/recipes/", models.Recipes.post_payload(), status.HTTP_201_CREATED)
     dict_elaborations=tests_helpers.client_post(self, self.client_authorized_1, "/api/elaborations/", models.Elaborations.post_payload(recipes=dict_recipes["url"]), status.HTTP_201_CREATED)
     
-    # # Check all
-    # tests_helpers.common_tests_Private(self,  '/api/elaborations_texts/', models.ElaborationsTexts.post_payload(elaborations=dict_elaborations["url"]),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
+    # CRUD relation ship one to one
+    print(dict_elaborations["url"]+"set_elaboration_text/")
+    dict_elaborations_text=tests_helpers.client_post(self, self.client_authorized_1, dict_elaborations["url"]+"set_elaboration_text/", models.ElaborationsTexts.post_payload(elaborations=dict_elaborations["url"]), status.HTTP_201_CREATED)
+    # dict_elaborations_text=tests_helpers.client_put(self, self.client_authorized_1, dict_elaborations_text["url"], models.ElaborationsTexts.post_payload(elaborations=dict_elaborations["url"], text="Updated text"), status.HTTP_200_OK)
+    # tests_helpers.client_get(self, self.client_authorized_1, dict_elaborations_text["url"], status.HTTP_200_OK, status.HTTP_200_OK)
+    # tests_helpers.client_delete(self, self.client_authorized_1, dict_elaborations_text["url"], status.HTTP_204_NO_CONTENT)
 
-    # Check recipe last update has changed upter post
-    dict_recipes_before=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
-    tests_helpers.client_post(self, self.client_authorized_1, "/api/elaborations_texts/", models.ElaborationsTexts.post_payload(elaborations=dict_elaborations["url"]), status.HTTP_201_CREATED)
-    dict_recipes_after=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
-    self.assertNotEqual(dict_recipes_before["last"], dict_recipes_after["last"])   
+    # # Check recipe last update has changed after post
+    # dict_recipes_before=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
+    # tests_helpers.client_post(self, self.client_authorized_1, "/api/elaborations_texts/", models.ElaborationsTexts.post_payload(elaborations=dict_elaborations["url"]), status.HTTP_201_CREATED)
+    # dict_recipes_after=tests_helpers.client_get(self, self.client_authorized_1, dict_recipes["url"], status.HTTP_200_OK)
+    # self.assertNotEqual(dict_recipes_before["last"], dict_recipes_after["last"])   
