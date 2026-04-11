f2feb4426b26739140494afdb5657f23643c250a
turulomio <turulomio@yahoo.es>
2026-04-11 20:07:33 +0200

    After recipes merge, remaining one updates its last modification datetime (#62)
    * After recipes merge, remaining one updates its last modification datetime
    
    * After recipes merge, remaining one updates its last modification datetime

--- calories_tracker/tests/test_recipes.py
+++ calories_tracker/tests/test_recipes.py
@@ -1,19 +1,11 @@
 from calories_tracker import models
-from . import tests_helpers, CaloriesTrackerAPITestCase
-from datetime import date, timedelta
-from django.test import tag
-from django.utils import timezone
-from json import loads
-from pydicts import lod
+from . import tests_helpers
 from rest_framework import status
-
-
-tag, models
-
 
 def test_recipes(self):
     tests_helpers.common_tests_Private(self,  '/api/recipes/', models.Recipes.post_payload(),  self.client_authorized_1, self.client_authorized_2, self.client_anonymous)
 
+def test_recipes_merge(self):
     #Merge recipes
     dict_recipe_main=tests_helpers.client_post(self, self.client_authorized_1,  "/api/recipes/", models.Recipes.post_payload(),  status.HTTP_201_CREATED)
     self.assertEqual(len(dict_recipe_main["elaborations"]), 0)
@@ -30,9 +22,13 @@ def test_recipes(self):
     self.assertEqual(dict_merged, "You should not pass the recipe that will remain in the list of recipes to be merged") 
     # Merge without main recipe on list
     dict_merged=tests_helpers.client_post(self, self.client_authorized_1, dict_recipe_main["url"]+"merge/", {"recipes":[dict_recipe_1["url"], dict_recipe_2["url"]]},  status.HTTP_200_OK)
+    
     ## Checks that have 2 elaborations and recipes_links
     self.assertEqual(len(dict_merged["elaborations"]), 2)
     self.assertEqual(len(dict_merged["recipes_links"]), 2)
+
+    # Check recipe last update has changed after merge
+    self.assertNotEqual(dict_recipe_main["last"], dict_merged["last"])
     
     ## Checks that merged recipes are deleted
     tests_helpers.client_get(self, self.client_authorized_1, dict_recipe_1["url"], status.HTTP_404_NOT_FOUND)        
